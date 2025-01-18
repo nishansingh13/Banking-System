8d132f2beb617ad8d0b805ee9117515df3d878f7
Nishan Singh <nishansingh2480@gmail.com>
2025-01-18 18:28:07 +0530

    checking something

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Learn More with real people
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
